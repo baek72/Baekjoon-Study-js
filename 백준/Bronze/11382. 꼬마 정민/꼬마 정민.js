819f587c4e12054a9197b22674aa46b6475e5885
BAEK JI EUN <140926812+baek72@users.noreply.github.com>
2023-07-31 20:03:47 +0900

    [Bronze V] Title: 꼬마 정민, Time: 156 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const inputData = fs.readFileSync(0, 'utf8').toString().split(' ');
+
+let A = Number(inputData[0]); 
+let B = Number(inputData[1]);
+let C = Number(inputData[2]);
+
+console.log(A+B+C);
