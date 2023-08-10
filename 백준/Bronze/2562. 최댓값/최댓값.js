ae1d4edb04d4bca3d62494b43b269cb1b10e0a16
BAEK JI EUN <140926812+baek72@users.noreply.github.com>
2023-08-10 19:39:58 +0900

    [Bronze III] Title: 최댓값, Time: 124 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.js
@@ -0,0 +1,5 @@
+const input = require("fs").readFileSync('/dev/stdin').toString().split('\n').map(x => Number(x));
+let max = Math.max(...input);
+
+console.log(max);
+console.log(input.indexOf(max) + 1);
