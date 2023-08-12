b4598b10e03b370b001b284a651fc572c91f2850
BAEK JI EUN <140926812+baek72@users.noreply.github.com>
2023-08-12 23:08:14 +0900

    [Bronze II] Title: 공 바꾸기, Time: 136 ms, Memory: 9356 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.js
@@ -0,0 +1,17 @@
+const input = require("fs").readFileSync('/dev/stdin').toString().split('\n');
+
+let [N, M] = input[0].split(" ").map(Number);
+let arr = new Array(N).fill(1).map((x, y) => x + y);
+
+// i = 줄 (공 넣는 횟수)
+for(let i = 1; i <= M; i++){
+  let [A, B] = input[i].split(" ").map(Number);
+  // A 복사
+  let tmp = arr[A - 1];
+  // A에는 B 
+  arr[A - 1] = arr[B - 1];
+  // B에는 A
+  arr[B - 1] = tmp;
+}
+// 문자로 변환
+console.log(arr.join(" "));
